8c596859f21fc1eb675b08a64b16989b1a9da931
Myriam Dubois <myriam.dubois@skema.edu>
2020-08-05 15:12:56 +0200

    index.jsx initialized + app & welcome msg component created

---
+++ src/components/app.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import WelcomeMessage from './welcome_message';
+
+class App extends Component {
+
+  render() {
+    return (
+      <div>
+        <WelcomeMessage />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/welcome_message.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class WelcomeMessage extends Component {
+  render() {
+    return (
+      <div className="container text-center">
+        <h1>Welcome to the Flickhouse quizz ! You'll be asked a series of "Yes or No" questions. Do your best!</h1>
+      </div>
+    );
+  }
+}
+
+export default WelcomeMessage;
